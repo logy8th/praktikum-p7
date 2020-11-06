33d0f6a2ce66d8fd3c5b06c9c82c4330e0394152
Fall <naufal.nirwansyah8@gmail.com>
2020-11-06 14:03:02 +0700

    lat2

---
+++ latihan2/lat2.py
@@ -0,0 +1,7 @@
+# mengimport library random
+from random import random
+# merandom bilangan riil sebanyak 5 kali
+for i in range(5):
+    bil = random()
+    # mencetak bilangan random
+    print(bil)
