153534e145ac1a3e901687d94b30213ff92f396a
Fall <naufal.nirwansyah8@gmail.com>
2020-11-07 23:20:52 +0700

    update lat2

--- latihan2/lat2.py
+++ latihan2/lat2.py
@@ -1,7 +1,7 @@
-# mengimport library random
-from random import random
-# merandom bilangan riil sebanyak 5 kali
-for i in range(5):
-    bil = random()
-    # mencetak bilangan random
-    print(bil)
+import random
+nilai = int(input('Masukan nilai n : '))
+a = 0
+for x in range (nilai):
+    i = random.uniform(.0,.5)
+    a+=1
+    print('data ke :',a,'==>', i)
